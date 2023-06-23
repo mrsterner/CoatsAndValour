e8dd205ac4d76a69855e58e3e7020aeb126d5eaa
mrsterner <simon@sterner.dev>
2023-06-23 16:01:44 +0200

    reformat code

--- src/main/java/dev/sterner/datagen/CAVModelProvider.java
+++ src/main/java/dev/sterner/datagen/CAVModelProvider.java
@@ -5,7 +5,6 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
-import net.minecraft.data.client.Model;
 import net.minecraft.data.client.Models;
 
 public class CAVModelProvider extends FabricModelProvider {
