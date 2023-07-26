ba58f4f8b77758a8e1c5646e6521cacc2767e519
⸸ ᴄʜᴀᴏᴛᴛɪᴄ ⸸ <64570529+chaottic@users.noreply.github.com>
2023-07-26 21:18:19 +1000

    you don't see this commit, its not real. close your eyes

--- src/main/java/dev/sterner/mixin/InGameHudMixin.java
+++ src/main/java/dev/sterner/mixin/InGameHudMixin.java
@@ -6,25 +6,30 @@ import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(InGameHud.class)
-public final class InGameHudMixin {
+public abstract class InGameHudMixin {
+    @Shadow protected abstract void drawHeart(DrawContext context, InGameHud.HeartType type, int x, int y, int v, boolean blinking, boolean halfHeart);
+
     @Unique
     private static final Identifier GANGRENE_HEARTS = CoatsAndValour.id("textures/gui/gangrene_hearts.png");
 
-    @Inject(method = "renderHealthBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;drawHeart(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/client/gui/hud/InGameHud$HeartType;IIIZZ)V", ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
-    public void renderHealthBar(DrawContext context, PlayerEntity player, int x, int y, int lines, int regeneratingHeartIndex, float maxHealth, int lastHealth, int health, int absorption, boolean blinking, CallbackInfo ci, InGameHud.HeartType heartType, int i, int j, int k, int l, int m, int n, int o, int p, int q) {
-        drawGangreneHeart(context, p, q, 0, 0, false, false);
-        ci.cancel();
+    @Inject(method = "renderHealthBar", at = @At("TAIL"))
+    public void renderHealthBar(DrawContext context, PlayerEntity player, int x, int y, int lines, int regeneratingHeartIndex, float maxHealth, int lastHealth, int health, int absorption, boolean blinking, CallbackInfo ci) {
+        drawGangreneHeart(context, null, x, y, 0, 0, false, false, false);
     }
 
     @Unique
-    private void drawGangreneHeart(DrawContext context, int x, int y, int u, int v, boolean blinking, boolean half) {
-        context.drawTexture(GANGRENE_HEARTS, x, y, u, v, 9, 9);
+    private void drawGangreneHeart(DrawContext context, InGameHud.HeartType type, int x, int y, int u, int v, boolean blinking, boolean half, boolean gangrene) {
+        if (gangrene) {
+            context.drawTexture(GANGRENE_HEARTS, x, y, u, v, 9, 9);
+        } else {
+            drawHeart(context, type, x, y, v, blinking, half);
+        }
     }
 }
